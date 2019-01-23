790844ba10853d6e3f9d06f92cf83324bb798d87
Christoffer Sommerfeld <35817456+ChristoffSomm@users.noreply.github.com>
2019-01-22 17:21:30 -0700

    HW2 Done :)

---
+++ HW2/script.js
@@ -0,0 +1,14 @@
+//Favorite Restaurants
+console.log('Asian Gardens');
+console.log('South 40');
+console.log('Pizza Hut');
+
+//Favorite Snack Foods
+console.log('Doritos');
+console.log('Jerky');
+console.log('Clif Bars');
+
+//Favorite Fruits 
+console.log('Bananas');
+console.log('Apples');
+console.log('Stawberries');
